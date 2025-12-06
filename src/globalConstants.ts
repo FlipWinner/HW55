8d204f7d1b65157e0acbf5a6fbec26955e2b0165
Okombaev Eldar <eldarokombaev@gmail.com>
2025-12-06 09:11:33 +0600

    Created GlobalConstants and types

---
+++ src/globalConstants.ts
@@ -0,0 +1,11 @@
+import meatImage from './assets/meat.png';
+import saladImage from './assets/salad.png';
+import cheeseImage from './assets/cheese.png';
+import bcnImage from './assets/bcn.png';
+
+export const INGREDIENTS: Ingredient[] = [
+	{ name: 'Meat', price: 80, image: meatImage },
+	{ name: 'Salad', price: 10, image: saladImage },
+	{ name: 'Cheese', price: 50, image: cheeseImage },
+	{ name: 'Bacon', price: 60, image: bcnImage }
+];
